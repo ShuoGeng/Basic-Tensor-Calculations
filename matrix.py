f645c83832da2f7326ad61583cf925ea95a1dca7
ShuoGeng <78362819+ShuoGeng@users.noreply.github.com>
2023-07-23 12:12:31 +0800

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,6 +1,13 @@
-#generate matrix
+#Calculate the ground state energy E0 and the first excited state energy E1 
+#for spin-1/2 antiferromagnetic Heisenberg chains of lengths L = 4, 6, 8, 10, 12, respectively 
+#where the hamitonian is defined as H = S_i * S_i+1 in periodic boundary condition
+#The energy gap for each length is defined as ∆ = E1 - E0, and caculate the gap
+#In this file, U(1) symmetry wull be used to simplified calculation
+
 import numpy as np
+
 def qN(L):
+    #caculate quantum numbers
     a = [1, -1]
     b = [1, -1]
     for n in range(L - 1):
@@ -13,6 +20,7 @@ def qN(L):
 
 
 def kronecker(A, B):
+    #The calculation of the Kronecker product.
     n1 = A.shape
     n2 = B.shape
     N1 = n1[0]
@@ -36,6 +44,7 @@ H = H.astype('int32')
 
 
 def hamiltonian(L):
+    # Calculate the exchange interaction terms in the Hamiltonian
     summary = np.zeros(2 ** L)
     summary = summary
     I = np.identity(2)
@@ -50,7 +59,7 @@ def hamiltonian(L):
         product = kronecker(list[0], list[1])
         for p in range(L - 3):
             product = kronecker(product, list[p + 2])
-        summary = summary + product
+        summary = summary + product # Periodic boundary conditions
 
     productx = kronecker(Sx, I)
 
@@ -75,6 +84,7 @@ def hamiltonian(L):
 
 
 def commute(L, Sz, Hs):
+    #Verify the commutation relations.
     summary0 = np.zeros(2 ** L)
     I = np.identity(2)
     for i in range(L):
@@ -91,16 +101,17 @@ def commute(L, Sz, Hs):
     Sz = summary0
     commute = Sz @ Hs - Hs @ Sz
     commute = commute.astype('float32')
-    print("链长取{}，哈密顿量与自旋算符的对易为".format(L))
+    print("For a chain with length {}, the commutation of the Hamiltonian with spin operators is".format(L))
     print(commute)
 
 
 for L in [4, 6, 8, 10, 12]:
     Hs = hamiltonian(L) / 4
     if L == 4:
-        print("链长取{}，系统的哈密顿量为".format(L))
+        print("For a chain with length {}, the Hamiltonian of the system is ".format(L))
         print(Hs)
 
+    #U(1) symmetry
     quantumNumber = qN(L)
     N = []
     for i in range(len(quantumNumber)):
@@ -121,10 +132,10 @@ for L in [4, 6, 8, 10, 12]:
         commute(L, Sz, Hs)
 
     if L == 4:
-        print("链长取{}，量子数为0对应的矩阵为".format(L))
+        print("For a chain with length{}，the matrix corresponding to a quantum number of 0 is".format(L))
         print(zeros)
-        print("本征值为")
+        print("The eigenvalue is")
         print(c0)
 
-    print("链长取{},基态能量为{}，第一激发态能量为{}，能隙为{}".
+    print("For a chain with length L = {}, the ground state energy is {}, the first excited state energy is {}, and the energy gap is {}.".
           format(L, c0[0], c0[1], c0[1] - c0[0]))
